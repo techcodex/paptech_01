4dd95a21be1590bd1f73a23c7f973f025af091b8
techcodex <sohaibamjad4212@gmail.com>
2018-11-29 13:19:29 +0500

    lecture 05

---
+++ lecture 5/array.php
@@ -0,0 +1,23 @@
+<?php
+//Two types or array in php
+//1.index array
+//2. Assoociative array
+//$mahin = array();
+/*
+$arr = [1,2,3,4,5,6,7,8,9,10];
+for($i = 0;$i<10;$i++) {
+	echo($arr[$i]."<br>");
+}
+*/
+$arr = [1,2,"abc","abc123",16.17];
+//for($i = 0;$i<4;$i++) {
+//	echo($arr[$i]."<br>");
+//}
+//echo("<pre>");
+//var_dump($arr);
+//echo("</pre>");
+/*
+int arr[] = new arr[5];
+arr['1',"abc",'45']; 
+*/
+?>
